eb4218ddaa8b68da8f34fa47225c76df8b42358c
bakseck <seck.bakar@ugb.edu.sn>
2023-11-15 10:47:11 +0000

    style: Update styles.css
    Updated the styles.css file to improve the appearance of the page. The changes include:
    
    - Revised styling for the 'i' element.
    - Removed styling for the 'Clear' button in the game-info section.
    
    These modifications enhance the visual aspects of the webpage and ensure a cleaner stylesheet.

--- js/index.js
+++ js/index.js
@@ -1,92 +1,73 @@
-// index.js
+const game = document.querySelector(".grid");
+const rows = 20;
+const cols = 20;
 
-// Variables pour définir la taille du tableau
-const rows = 30;
-const cols = 30;
+const grid = new Array(rows).fill(null).map(() => new Array(cols).fill(false));
 
-// Création du tableau pour représenter le jeu
-let grid;
-
-// Fonction pour initialiser le tableau avec des cellules aléatoires vivantes
-function initializeGrid() {
-    grid = new Array(rows);
+function initGrid() {
     for (let i = 0; i < rows; i++) {
-        grid[i] = new Array(cols);
-        for (let j = 0; j < cols; j++) {
-            grid[i][j] = Math.random() > 0.7 ? 1 : 0; // 30% de chances d'être vivant
-        }
+      for (let j = 0; j < cols; j++) {
+        grid[i][j] = Math.random() < 0.5;
+      }
     }
 }
 
-// Fonction pour afficher le jeu dans le conteneur HTML
-function displayGrid() {
-    const gameContainer = document.getElementById("game-container");
-    gameContainer.innerHTML = "";
+function renderGrid() {
+    game.innerHTML = '';
+
     for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell", grid[i][j] ? "alive" : "dead");
-            cell.style.background = grid[i][j] ? 'red' : 'green'
-            cell.addEventListener("click", () => toggleCell(i, j));
-            gameContainer.appendChild(cell);
-        }
+      for (let j = 0; j < cols; j++) {
+        const cellDiv = document.createElement('div');
+        cellDiv.classList.add('cell');
+        cellDiv.style.backgroundColor = grid[i][j] ? '#333' : '#fff';
+        game.appendChild(cellDiv)
+      }
+      game.appendChild(document.createElement('br'));
     }
 }
 
-// Fonction pour faire évoluer le jeu d'une génération
-function evolve() {
-    const newGrid = new Array(rows);
+function updateGrid() {
+    const newGrid = new Array(rows).fill(null).map(() => new Array(cols).fill(false));
+
     for (let i = 0; i < rows; i++) {
-        newGrid[i] = new Array(cols);
-        for (let j = 0; j < cols; j++) {
-            const neighbors = countNeighbors(i, j);
-            if (grid[i][j] === 1) {
-                newGrid[i][j] = neighbors === 2 || neighbors === 3 ? 1 : 0;
-            } else {
-                newGrid[i][j] = neighbors === 3 ? 1 : 0;
-            }
+      for (let j = 0; j < cols; j++) {
+        const neighbors = countNeighbors(i, j);
+        if (grid[i][j]) {
+          newGrid[i][j] = neighbors === 2 || neighbors === 3;
+        } else {
+          newGrid[i][j] = neighbors === 3;
         }
+      }
     }
-    grid = newGrid;
+
+    grid.splice(0, grid.length, ...newGrid);
 }
 
-// Fonction pour compter le nombre de voisins vivants d'une cellule
 function countNeighbors(row, col) {
     let count = 0;
+
     for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-            const newRow = row + i;
-            const newCol = col + j;
-            if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
-                count += grid[newRow][newCol];
-            }
+      for (let j = -1; j <= 1; j++) {
+        const newRow = row + i;
+        const newCol = col + j;
+
+        if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && !(i === 0 && j === 0)) {
+          count += grid[newRow][newCol] ? 1 : 0;
         }
+      }
     }
-    count -= grid[row][col]; // Soustraire la cellule centrale
+
     return count;
 }
 
-function toggleCell(row, col) {
-    grid[row][col] = 1 - grid[row][col];
-    displayGrid();
-}
-
-// Fonction pour démarrer le jeu
-function startGame() {
+function animateGame() {
+    initGrid();
     setInterval(() => {
-        evolve();
-        displayGrid();
-    }, 1000); // Mettez à jour toutes les secondes (ajustez selon vos préférences)
+      renderGrid();
+      updateGrid();
+    }, 500);
 }
 
-// Fonction pour effacer le jeu
-function clearGame() {
-    grid = new Array(rows).fill(0).map(() => new Array(cols).fill(0));
-    displayGrid();
-}
+animateGame();
 
-// Initialisation du jeu lors du chargement de la page
-document.addEventListener("DOMContentLoaded", () => {
-    initializeGrid();
-    displayGrid();
-});
+console.log(grid);
